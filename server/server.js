7c2b32449ecac7c85916ab3c8fbd4e84d79819d5
Andrew Falcon <falconandrew2@gmail.com>
2024-06-02 13:47:20 -0700

    convert url to video

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
 const app = express();
 const port = 5001;
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/download-video', (req, res) => {
+app.post('/download-video', async (req, res) => {
     console.log(req.body.url);
-    res.json({message: "Post successful!!!"})
+    const info = await ytdl.getInfo(req.body.url);
+    const videoStream = ytdl.downloadFromInfo(info);
+    const writeStream = fs.createWriteStream('video.mp4');
+    videoStream.pipe(writeStream);  
 })
 
 app.listen(port, () => {
